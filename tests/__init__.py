fd2dcb7fa8f6040a5f205cb484448c93fc70a49f
AAAlvesJr <aalvesju@gmail.com>
2025-10-11 04:41:54 -0300

    Adding unit test for valid xdi file. Addressing the issue #2 .

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,6 +3,7 @@ import unittest
 from tests.test_WrongFields import *
 from tests.test_MissingFields import *
 from tests.test_MissingEndOfHeader import *
+from tests.test_ValidXDI import *
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
